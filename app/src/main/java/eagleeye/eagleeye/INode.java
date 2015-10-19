ca09897cdbd90598f86bcc81389273dd0b644562
Jcel <c16james.combs@usafa.edu>
2015-10-19 13:16:36 -0400

    Database Fall Break Commit

--- app/src/main/java/eagleeye/eagleeye/INode.java
+++ app/src/main/java/eagleeye/eagleeye/INode.java
@@ -2,30 +2,56 @@ package eagleeye.eagleeye;
 
 /**
  * Created by Mike R on 10/6/2015.
+ * Edited by James Combs on 10/17/2015.
  */
 public class INode {
 
+    //Class attributes
+    //Image image: Image held in node
     private Image image;
+    //String name: Name of building associated with image
     private String name;
+    //INode next: Next INode in the linked list
     private INode next;
 
+    /**
+     * Basic Constructor
+     * @param n Name of the building associated with image
+     * @param i Image to be held in INode
+     */
     public void INode(String n, Image i){
         name = n;
         image = i;
     }
 
+    /**
+     * Provides a link to next INode in linked list
+     * @param a INode to be linked to
+     */
     public void setNext(INode a){
         next = a;
     }
 
+    /**
+     * Returns the image stored in INode
+     * @return Returns the image
+     */
     public Image getImage(){
         return this.image;
     }
 
+    /**
+     * Returns the building name stored in INode
+     * @return Returns the building name
+     */
     public String getName(){
         return this.name;
     }
 
+    /**
+     * Returns the next INode
+     * @return Returns the next INode
+     */
     public INode getNext(){
         return this.next;
     }
